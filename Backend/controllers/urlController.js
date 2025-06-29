3336fe01c469e119963a8b444fe051761b6205a5
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-29 12:50:37 +0500

    added get-by-id router

--- Backend/controllers/urlController.js
+++ Backend/controllers/urlController.js
@@ -22,3 +22,25 @@ const createURL = async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 };
+
+const getUrlById = async (req, res) => {
+    const { shortUrl } = req.params;
+    try {
+        const urlEntry = await URL.findOne({ shortUrl })
+        if (!urlEntry) {
+            return res.status(404).json({ error: 'Short URL not found' });
+        }
+        await URL.updateOne({ shortUrl }, {
+            $inc: { visits: 1 },
+            $push: { clickTimestamps: new Date() }
+        })
+          res.redirect(urlEntry.originalUrl);
+    } catch (error) {
+        res.status(500).json({ error: 'Redirection error' });
+    }
+}
+
+module.exports = {
+    createURL,
+    getUrlById
+}
