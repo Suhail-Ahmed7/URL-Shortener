6099520e4b06f6695bc95bd3cde1b9452cdfc264
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-29 13:03:16 +0500

    server.js file completed..

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const connectDB = require('./config/dbConnection')
 const dotenv = require('dotenv');
-
+const urlRoutes = require('./routes/urlRoutes')
 dotenv.config();
 connectDB()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
-
+app.use('/api',urlRoutes)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
