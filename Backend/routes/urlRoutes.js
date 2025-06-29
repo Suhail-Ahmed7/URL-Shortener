1ed539a80fe9b05ae5c49bb12338c97f828f803f
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-29 12:53:16 +0500

    added routes file..

---
+++ Backend/routes/urlRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { createURL, getUrlById } = require('../controllers/urlController');
+
+router.post('/shorten', createURL);
+router.get('/:shortUrl', getUrlById);
